382df4c1ca661b7170c21dca4e3721f780cbb630
Philipp Bouzid <ph.bouzid03@gmail.com>
2026-01-18 16:54:38 +0500

    fix: resolve vulnerabilities (#171)
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Sergey Ivanov <delta_001@bk.ru>

--- monitoring/main.go
+++ monitoring/main.go
@@ -25,7 +25,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/segmentio/kafka-go"
+	kafka "github.com/segmentio/kafka-go"
 	"github.com/segmentio/kafka-go/sasl"
 	"github.com/segmentio/kafka-go/sasl/scram"
 )
@@ -133,9 +133,7 @@ func main() {
 		Resources: resources,
 	}
 
-	configs := &kafka.DescribeConfigsResponse{}
-
-	configs, err = client.DescribeConfigs(context.Background(), request)
+	configs, err := client.DescribeConfigs(context.Background(), request)
 	if err != nil {
 		log.Fatalf("Failed to describe configs: %s", err)
 	}
@@ -227,7 +225,10 @@ func CalcSkewNFormMetric(topics []kafka.Topic, result map[string]string) string 
 	metrics := new(bytes.Buffer)
 	for _, metricPerBroker := range result {
 		if strings.Contains(metricPerBroker, "kafka,broker=") {
-			fmt.Fprintf(metrics, "%s\n", metricPerBroker)
+			_, err := fmt.Fprintf(metrics, "%s\n", metricPerBroker)
+			if err != nil {
+				return ""
+			}
 		}
 	}
 	return metrics.String()
